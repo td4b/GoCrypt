2cff9c3ef345470ac459ba3df4619a9982e94e73
td4b <edwin.twest@gmail.com>
2019-01-09 23:15:23 -0800

    Update createdb.go

--- createdb.go
+++ createdb.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	db, _ := sql.Open("sqlite3", "./data.db")
 	sqlStmt := `
-  create table filemaps (id integer not null primary key, fileid text, ipfshash text);
+        create table filemaps (id integer not null primary key, fileid text, ipfshash text);
 	delete from filemaps;
 	`
 	query, _ := db.Prepare(sqlStmt)
